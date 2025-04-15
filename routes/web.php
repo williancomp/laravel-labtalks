8a0672056a8384c31b6fd8c914067eabbc92e2bb
Willian Garcias <williangarcias10@gmail.com>
2025-04-15 13:54:37 -0300

    redis2

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/redis-test', function () {
+    try {
+        $ping = Illuminate\Support\Facades\Redis::ping();
+        return "Conexão com Redis OK! Resposta: " . $ping;
+    } catch (\Exception $e) {
+        return "Erro na conexão com Redis: " . $e->getMessage();
+    }
+});
